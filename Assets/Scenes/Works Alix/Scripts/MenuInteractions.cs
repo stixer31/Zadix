c791613783e04f98264dc2ac9db502810debc030
xHappiness <nebotieff.alexandre@live.fr>
2019-01-28 15:14:32 +0100

    Rajouts d'éléments au menu

---
+++ Assets/Scenes/Works Alix/Scripts/MenuInteractions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInteractions : MonoBehaviour
+{
+
+    public GameObject startMenu;
+    public bool showingMenu;
+    public float axisCancel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        axisCancel = Input.GetAxisRaw("Cancel");
+
+        if (axisCancel == 1)
+            {
+            showingMenu = !showingMenu;
+            startMenu.SetActive(showingMenu);
+        }
+    }
+}
