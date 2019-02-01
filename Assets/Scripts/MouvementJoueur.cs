7e18f4cea11e5907a4f51f0ad61c9936461765b3
stixer31 <weblyne.firme@gmail.com>
2019-02-01 21:44:21 +0100

    Personage

---
+++ Assets/Scripts/MouvementJoueur.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MouvementJoueur : MonoBehaviour
+{
+
+    public float rotateSpeed = 20f;
+    public float walkSpeed = 0.5f;
+
+    private Vector3 targetPosition;
+    private bool isMoving;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMoving = true;
+            SetTargetPosition();
+        }
+
+        if (isMoving)
+            MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(targetPosition - transform.position);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, walkSpeed * Time.deltaTime);
+
+        if (transform.position == targetPosition)
+            isMoving = false;
+    }
+
+    private void SetTargetPosition()
+    {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0.0f;
+
+        if (playerPlane.Raycast(ray, out hitdist))
+            targetPosition = ray.GetPoint(hitdist);
+    }
+}
