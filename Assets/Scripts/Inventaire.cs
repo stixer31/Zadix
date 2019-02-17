7200d8a3a2910711ac6d950549cf127b09094fe6
stixer31 <weblyne.firme@gmail.com>
2019-02-17 23:25:35 +0100

    inventaiure2

---
+++ Assets/Scripts/Inventaire.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventaire : MonoBehaviour
+{
+    public GameObject Inventaire_Joueur;
+    bool locked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inventaire_Joueur.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool Inventaire = Input.GetButtonDown("Inventaire");
+        bool Competences = Input.GetButton("Competences");
+        bool Escape = Input.GetButtonUp("Escape");
+        
+        if (Inventaire)
+        {
+            Inventaire_Joueur.active = locked;
+            locked = !locked;
+        }
+        else if (Competences)
+        {
+            
+        }
+        else if (Escape)
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+}
