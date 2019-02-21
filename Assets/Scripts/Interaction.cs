6e561049f9f6d002f3f9e002c04aace217610cb8
stixer31 <weblyne.firme@gmail.com>
2019-02-21 22:59:10 +0100

    interaction personnage

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interaction : MonoBehaviour
+{
+    private Text TxtInfos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TxtInfos = GameObject.Find("MessageTXT").GetComponent<Text>();
+        //TxtInfos = gameObject.transform.Find("MessageTXT").GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Verifie l'objet touché
+    void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag == "Equipements")
+        {
+            TxtInfos.text = "en contact je eux agir sur l'objet";
+            print("dedans");
+        }
+    }
+
+    void OnTriggerExit(Collider Col)
+    {
+        if (Col.gameObject.tag == "Equipements")
+        {
+            TxtInfos.text = "";
+            print("dehors");
+        }
+    }
+}
